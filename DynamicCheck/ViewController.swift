27c44e143b0a7d85640e9c61ddc510b74ef9249c
Alexey Agapov <agapov.one@gmail.com>
2022-11-24 13:56:58 +0500

    Add vc logic

--- DynamicCheck/ViewController.swift
+++ DynamicCheck/ViewController.swift
@@ -9,11 +9,77 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var animator = UIDynamicAnimator(referenceView: view)
+    lazy var radialTouchGravityBehaviour = UIFieldBehavior.radialGravityField(position: .zero)
+    lazy var collision = UICollisionBehavior(items: views)
+    let views = [
+        newPin(color: .red),
+        newPin(color: .green),
+        newPin(color: .blue)
+    ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tap)))
+
+        let switchView = UISwitch(frame: .zero, primaryAction: UIAction { _ in
+            self.collision.translatesReferenceBoundsIntoBoundary.toggle()
+        })
+
+        switchView.center = CGPoint(x: view.center.x, y: 80)
+        view.addSubview(switchView)
+    }
+
+    @objc private func tap() {
+        views.forEach {
+            $0.removeFromSuperview()
+        }
+        animator.removeAllBehaviors()
+        createNew()
+    }
+
+    private func createNew() {
+        views.forEach {
+            view.addSubview($0)
+
+            $0.frame = CGRect(x: 0, y: 0, width: .random(in: 80..<400), height: .random(in: 100..<300))
+            $0.center = view.center
+//                .applying(CGAffineTransform(translationX: CGFloat.random(in: -50..<50), y: CGFloat.random(in: -50..<50)))
+//            $0.translatesAutoresizingMaskIntoConstraints = false
+//            NSLayoutConstraint.activate([
+//                $0.widthAnchor.constraint(equalToConstant: 30),
+//                $0.heightAnchor.constraint(equalToConstant: 30)
+//            ])
+        }
+
+        animator.addBehavior(collision)
+
+//        radialTouchGravityBehaviour.minimumRadius = 100
+//        radialTouchGravityBehaviour.falloff = 3
+//        animator.addBehavior(radialTouchGravityBehaviour)
     }
 
 
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        radialTouchGravityBehaviour.position = (touches.first?.location(in: view))!
+//        animator.addBehavior(radialTouchGravityBehaviour)
+//    }
+//
+//    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        radialTouchGravityBehaviour.position = (touches.first?.location(in: view))!
+//    }
+//
+//    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        animator.removeBehavior(radialTouchGravityBehaviour)
+//    }
+//
+//    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        animator.removeBehavior(radialTouchGravityBehaviour)
+//    }
 }
 
+private func newPin(color: UIColor) -> UIView {
+    let v = UIView()
+    v.backgroundColor = color
+    return v
+}
